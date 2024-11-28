f5e51c74b91ce4cf13d46fbd4d527ed3d6eaf4a2
lifintsefv <157622370+Llifintsefv@users.noreply.github.com>
2024-11-28 16:11:27 +0300

    added prepare statement to all methods

--- internal/rss/repository/repository.go
+++ internal/rss/repository/repository.go
@@ -44,14 +44,20 @@ func (r *rssRepository) SaveNews(feed  *gofeed.Feed) error{
 }
 
 func (r *rssRepository) GetNewsById(id int) (models.News,error) {
-	rows,err := r.db.Query("SELECT title, link, published, description FROM news WHERE id = $1",id)
+	stmt,err := r.db.Prepare("SELECT id,title, link, published, description FROM news WHERE id = $1")
+	if err != nil {
+		return models.News{},fmt.Errorf("failte to prepare statement: %w",err)
+	}
+	defer stmt.Close()
+	rows,err :=stmt.Query(id)
+	
 	if err != nil {
 		return models.News{},err
 	}
 	defer rows.Close()
 	var news models.News
 	if rows.Next(){
-		err = rows.Scan(&news.Title, &news.Link, &news.Published, &news.Description)
+		err = rows.Scan(&news.ID,&news.Title, &news.Link, &news.Published, &news.Description)
 		if err != nil {
 			return models.News{},err
 		}
@@ -60,20 +66,30 @@ func (r *rssRepository) GetNewsById(id int) (models.News,error) {
 	return models.News{},fmt.Errorf("id not found %d",id)
 }
 
-func(r *rssRepository) GetAllNews() ([]models.News,error) {
-	rows,err := r.db.Query("SELECT title, link, published, description FROM news")
+func (r *rssRepository) GetAllNews() ([]models.News, error) {
+	stmt, err := r.db.Prepare("SELECT title, link, published, description FROM news")
 	if err != nil {
-		return []models.News{},err
+		return []models.News{}, fmt.Errorf("failed to prepare statement: %w", err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return []models.News{}, fmt.Errorf("failed to execute query: %w", err)
 	}
 	defer rows.Close()
-	var news []models.News
-	for rows.Next(){
+
+	var newsList []models.News
+	for rows.Next() {
 		var n models.News
 		err = rows.Scan(&n.Title, &n.Link, &n.Published, &n.Description)
 		if err != nil {
-			return []models.News{},err
+			return []models.News{}, fmt.Errorf("failed to scan news: %w", err)
 		}
-		news = append(news,n)
+		newsList = append(newsList, n)
 	}
-	return news, nil
+	if err := rows.Err(); err != nil {
+		return []models.News{}, fmt.Errorf("failed to iterate rows: %w", err)
+	}
+	return newsList, nil
 }
